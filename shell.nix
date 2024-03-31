be4e4f5b0adc6435f7e0d2794840634272ee3fc5
Kristof Ocvirk <ocvirk.kristof@gmail.com>
2024-03-31 18:51:29 +0200

    add spec

---
+++ shell.nix
@@ -0,0 +1,23 @@
+let
+ pkgs = import <nixpkgs> {};
+ # choose the ocaml version you want to use
+ ocamlPackages = pkgs.ocaml-ng.ocamlPackages_4_12;
+in
+pkgs.mkShell {
+
+  # build tools
+  nativeBuildInputs = with ocamlPackages; [ ocaml findlib dune_3 ocaml-lsp ];
+
+  # dependencies
+  buildInputs = with ocamlPackages; [ ocamlgraph menhir ocaml-vdom ];
+
+  packages = with pkgs; [ (vscode-with-extensions.override {
+    vscodeExtensions = with vscode-extensions; [
+      ocamllabs.ocaml-platform
+      vscodevim.vim
+    ]; })
+    opam
+    ocamlformat
+    ocamlPackages.utop
+  ];
+}
